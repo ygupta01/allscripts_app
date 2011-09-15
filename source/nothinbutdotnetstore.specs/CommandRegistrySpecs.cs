7c7568143adb88e46b74d8a91e37694d320e0481
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-09-15 10:28:50 -0600

    Updated

--- source/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
+++ source/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
@@ -42,6 +42,30 @@ namespace nothinbutdotnetstore.specs
             static IContainRequestInformation request;
             static IList<IProcessARequest> all_the_commands;
         }
+
+        public class when_finding_the_command_that_can_process_a_request_and_it_does_not_have_the_command : concern
+        {
+            Establish c = () =>
+            {
+                the_special_case = fake.an<IProcessARequest>();
+                all_the_commands = Enumerable.Range(1, 100).Select(x => fake.an<IProcessARequest>()).ToList();
+                request = fake.an<IContainRequestInformation>();
+
+                depends.on<IEnumerable<IProcessARequest>>(all_the_commands);
+                depends.on(the_special_case);
+            }
+
+            Because b = () =>
+                result = sut.get_the_command_that_can_process(request);
+
+            It should_return_the_special_case_to_the_caller = () =>
+                result.ShouldEqual(the_special_case);
+
+            static IProcessARequest result;
+            static IContainRequestInformation request;
+            static IList<IProcessARequest> all_the_commands;
+            static IProcessARequest the_special_case;
+        }
     }
 
 }
--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -1,13 +1,20 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+
 namespace nothinbutdotnetstore.web.core
 {
     public class CommandRegistry : ICanFindCommands
     {
+        IEnumerable<IProcessARequest> all_commands;
+
+        public CommandRegistry(IEnumerable<IProcessARequest> all_commands)
+        {
+            this.all_commands = all_commands;
+        }
 
         public IProcessARequest get_the_command_that_can_process(IContainRequestInformation the_request)
         {
-           return this.get_the_command_that_can_process(the_request);          
-           
+            return all_commands.First(x => x.can_handle(the_request));
         }
     }
 }
